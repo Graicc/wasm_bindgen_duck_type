d42665015eafa257462ffd2c8b183f31df1bc135
Graicc <33105645+Graicc@users.noreply.github.com>
2022-10-05 01:56:18 -0400

    Add constructor and default trait implementation

--- wasm_bindgen_duck_type_macro_impl/src/lib.rs
+++ wasm_bindgen_duck_type_macro_impl/src/lib.rs
@@ -14,9 +14,9 @@ pub fn wasm_bindgen_duck_type(_attr: TokenStream, item: TokenStream) -> TokenStr
 fn impl_duck_type(item_struct: &syn::ItemStruct) -> TokenStream {
     let name = &item_struct.ident;
     let vis = &item_struct.vis;
-    let feilds = &item_struct.fields;
-    if let syn::Fields::Named(feilds) = feilds {
-        let feilds = feilds.named.iter().map(|x| {
+    let fields = &item_struct.fields;
+    if let syn::Fields::Named(feilds) = fields {
+        let field_methods = feilds.named.iter().map(|x| {
             let f_name = x.ident.as_ref().unwrap();
             let f_type = &x.ty;
 
@@ -30,12 +30,46 @@ fn impl_duck_type(item_struct: &syn::ItemStruct) -> TokenStream {
             }
         });
 
+        let signature = feilds.named.iter().map(|x| {
+            let f_name = x.ident.as_ref().unwrap();
+            let f_type = &x.ty;
+
+            quote! {
+                #f_name: #f_type
+            }
+        });
+
+        let new_impl = feilds.named.iter().map(|x| {
+            let f_name = x.ident.as_ref().unwrap();
+            let prop = format_ident!("set_{}", f_name);
+
+            quote! {
+                new.#prop(#f_name);
+            }
+        });
+
         let gen = quote! {
             #[::wasm_bindgen::prelude::wasm_bindgen(method, getter)]
             extern "C" {
                 #vis type #name;
 
-                #(#feilds)*
+                #(#field_methods)*
+            }
+
+            impl #name {
+                pub fn new(#(#signature),*) -> Self {
+                    let new: Self = Self::default();
+
+                    #(#new_impl)*
+
+                    new
+                }
+            }
+
+            impl ::std::default::Default for #name {
+                fn default() -> Self {
+                    JsValue::from(js_sys::Object::new()).into()
+                }
             }
         };
 
